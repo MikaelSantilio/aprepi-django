c9c5c66a2cbaf7feef0752b9558872750fa3b345
MikaelSantilio <mikael.santilio@gmail.com>
2021-01-15 11:06:24 -0300

    added state and city models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class State(models.Model):
+    name = models.CharField(max_length=75)
+    uf = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
+
+class City(models.Model):
+    state = models.ForeignKey(State, related_name='cities', on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return f"{self.state.uf} - {self.name}"
--- users/models.py
+++ users/models.py
@@ -1,42 +1,48 @@
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import RegexValidator
-from django.db import models
-from users.validators import validate_CPF
+# from django.contrib.auth.models import AbstractUser
+# from django.core.validators import RegexValidator
+# from django.db import models
+# from users.validators import validate_CPF
+# from core.models import State, City
 
 
-class User(AbstractUser):
-    is_employee = models.BooleanField(default=False)
-    is_member = models.BooleanField(default=False)
-    is_benefactor = models.BooleanField(default=False)
-    is_voluntary = models.BooleanField(default=False)
+# # class User(AbstractUser):
+# #     is_employee = models.BooleanField(default=False)
+# #     is_member = models.BooleanField(default=False)
+# #     is_benefactor = models.BooleanField(default=False)
+# #     is_voluntary = models.BooleanField(default=False)
 
 
-class UserCommonFields(models.Model):
-    cpf = models.CharField("CPF", unique=True, max_length=14, validators=[validate_CPF])
-    birth_date = models.DateField()
-    phone_regex = RegexValidator(
-        regex=r'(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})',
-        message="Phone number must be entered in the format: '99 99999-9999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+# class UserCommonFields(models.Model):
+#     cpf = models.CharField("CPF", unique=True, max_length=14, validators=[validate_CPF])
+#     birth_date = models.DateField()
+#     phone_regex = RegexValidator(
+#         regex=r'(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})',
+#         message="Phone number must be entered in the format: '99 99999-9999'. Up to 15 digits allowed.")
+#     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
 
 
-class Employee(UserCommonFields):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+# class Employee(UserCommonFields):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
 
-class Member(UserCommonFields):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+# class Member(UserCommonFields):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
 
-class MemberDetail(UserCommonFields):
-    member = models.OneToOneField(Member, on_delete=models.CASCADE, primary_key=True)
-    father_name = models.CharField(max_length=64, blank=True, null=True)
-    mother_name = models.CharField(max_length=64, blank=True, null=True)
+# class MemberDetail(UserCommonFields):
+#     member = models.OneToOneField(Member, on_delete=models.CASCADE, primary_key=True)
+#     father_name = models.CharField(max_length=64, blank=True, null=True)
+#     mother_name = models.CharField(max_length=64, blank=True, null=True)
+#     companion_name = models.CharField(max_length=64, blank=True, null=True)
+#     state = models.ForeignKey(State, on_delete=models.CASCADE)
+#     city = models.ForeignKey(City, on_delete=models.CASCADE)
+#     street = models.ForeignKey()
 
 
-class Benefactor(UserCommonFields):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+# class Benefactor(UserCommonFields):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
 
-class Voluntary(UserCommonFields):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+# class Voluntary(UserCommonFields):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
