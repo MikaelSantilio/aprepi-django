dede116707e787ce58714591f83781fc36b3b03b
RafaGates <rafael12silva73@gmail.com>
2021-01-24 19:28:03 -0300

    criando models de evento

--- evento/models.py
+++ evento/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from users.models import Base, Voluntary
 
-# Create your models here.
+
+class Event(Base):
+    event_name = models.CharField('Nome do Evento', max_length=150, blank=True, null=True)
+    date_event = models.DateTimeField()
+
+    volunteers = models.ManyToManyField(
+        Voluntary,
+        verbose_name='volunteers',
+        related_name="evento_set",)
+    event_details = models.TextField()
+    materials = models.TextField()
+
+
+class Cost(Base):
+    cost = models.FloatField('R$0.00')
+    details = models.CharField('Detalhes do gasto', max_length=200, blank=True, null=True)
+    event = models.ManyToManyField(
+        Event,
+        verbose_name='event',
+        related_name="cost_set",)
+
+
+class Collection(Base):
+    collection = models.FloatField('R$0.00')
+    details = models.CharField('Detalhes do gasto', max_length=200, blank=True, null=True)
+    event = models.ManyToManyField(
+        Event,
+        verbose_name='event',
+        related_name="collection_set",)
