daeee4af4c9ee3355653c1b4121e6cb4ecf765dd
MikaelSantilio <mikael.santilio@gmail.com>
2021-06-15 16:13:14 -0300

    added leaders templates

--- aprepi/settings.py
+++ aprepi/settings.py
@@ -65,6 +65,12 @@ TEMPLATES = [
         'DIRS': [str(BASE_DIR / "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
+            # https://docs.djangoproject.com/en/dev/ref/settings/#template-loaders
+            # https://docs.djangoproject.com/en/dev/ref/templates/api/#loader-types
+            "loaders": [
+                "django.template.loaders.filesystem.Loader",
+                "django.template.loaders.app_directories.Loader",
+            ],
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
